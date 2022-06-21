97098484ad630c6c2460cd4aff709da5ee6adb86
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-21 13:50:24 +0430

    Configuring the project

--- MRJ/urls.py
+++ MRJ/urls.py
@@ -1,4 +1,4 @@
-"""MRJ URL Configuration
+"""MyResume URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from website.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", index_view, name="index")
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
