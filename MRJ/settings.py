c8fac013456a462766e128e94db3ec70397cb42c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-22 10:51:59 +0430

    Configuring settings.py

--- MRJ/settings.py
+++ MRJ/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$w1$fix46(^vpix=thafi!xqjic5nv5=y05=agp(!v36yxrjx+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "mrj-elns.fandogh.cloud"]
 
 
 # Application definition
